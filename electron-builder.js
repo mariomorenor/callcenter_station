5fc461973e50d89e2c042516aca2dd3f0fd2ab42
Jonathan Moreno <mario_morenor@hotmail.com>
2022-10-20 22:59:48 -0500

    Agregado Configuración Para compilar el producto

---
+++ electron-builder.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "appId": "com.callcenter.station",
+    "productName": "CallCenter Station",
+    "copyright": "Copyright © year ${author}",
+    "linux": {
+        "target": "deb",
+        // "icon": "public/img/icons/logo_256x256.ico"
+    },
+}
